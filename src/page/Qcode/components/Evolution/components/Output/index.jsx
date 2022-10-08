8e8601d6d54fa6c54d3833dcecd4645867c0c0f1
胡光泽 <2581504687@qq.com>
2022-10-08 17:07:17 +0800

    首次部署到线上

---
+++ src/page/Qcode/components/Evolution/components/Output/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import OutputRect from './components/OutputRect'
+export default function Output(props) {
+    // console.log(props, 'output')
+    let basesData =[0]
+    if (props.outputData){
+      basesData = props.outputData.bases
+    }
+      return (
+        // <g></g>
+        <g transform={`translate(${basesData.length * 26 + 10},90)`}>
+          {/* <rect width={32} height={32} fill="green"></rect> */}
+          {basesData.map((basesData, index) => (
+            <OutputRect
+              basesData={basesData}
+              key={index}
+              index={index}
+            ></OutputRect>
+          ))}
+        </g>
+      )
+}
